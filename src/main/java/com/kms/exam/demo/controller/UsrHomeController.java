922781d6b3118126505555855042bb9b9543e852
kimminsic <alstlr1135@gmail.com>
2022-06-28 18:27:45 +0900

    count값 조회및 변경

--- src/main/java/com/kms/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kms/exam/demo/controller/UsrHomeController.java
@@ -12,34 +12,17 @@ public class UsrHomeController {
 	public UsrHomeController() {
 		count=0;
 	}
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	}
 	
-	@RequestMapping("/usr/home/main2")
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}
-	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
-	}
-	
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		return count++;
-	}
-	
-	@RequestMapping("/usr/home/main5")
-	@ResponseBody
-	public int showMain5() {
-		count=0;
+	public int getCount() {
 		return count;
+	}
+	
+	@RequestMapping("/usr/home/doSetCount")
+	@ResponseBody
+	public String doSetCount(int count) {
+		this.count=count;
+		return "count의 값"+this.count+"로 초기화 되었습니다.";
 	}
 }
