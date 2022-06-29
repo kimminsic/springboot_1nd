6de9327c9e27c06fdb7133952a7e6e0efc05cb45
kimminsic <alstlr1135@gmail.com>
2022-06-29 20:40:03 +0900

    마이바티스, MySQL JDBC 드라이버 추가, 설정 파일에 DB접속정보 기입, 테스트

--- src/main/java/com/kms/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kms/exam/demo/repository/ArticleRepository.java
@@ -1,62 +1,30 @@
 package com.kms.exam.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.kms.exam.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	private int articlesLastId;
-	private List<Article> articles;
-	public ArticleRepository() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-	}
+@Mapper
+public interface ArticleRepository {
 	
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			writeArticle(title,body);
-
-		}
-	}
-
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articlesLastId = id;
-
-		return article;
-	}
-
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
+	//INSERT INTO article SET title = ?, `body` = ?, regDate = NOW(), updateDate = NOW();
 	
+	public Article writeArticle(String title, String body);
 
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	public List<Article> getArticles() {
-		return articles;
-	}
+	//SELECT * FROM article WHERE id = ?;
+	@Select("select * from article where id = #{id}")
+	public Article getArticle(@Param("id") int id);
+	
+	//DELETE FROM article WHERE id = ?;
+	public void deleteArticle(int id);
+	
+	//UPDATE article SET title = ?, `body` =  ? , updateDate = NOW() WHERE id = ?;
+	public void modifyArticle(int id, String title, String body);
+	
+	//SELECT * FROM article ORDER BY DESC;
+	public List<Article> getArticles();
 }
