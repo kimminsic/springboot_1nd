c491e5004d7855724e80c2abfa813fe8c02c97f3
kimminsic <alstlr1135@gmail.com>
2022-06-29 19:21:36 +0900

    ArticleService=>ArticleRepository

--- src/main/java/com/kms/exam/demo/service/ArticleService.java
+++ src/main/java/com/kms/exam/demo/service/ArticleService.java
@@ -1,65 +1,48 @@
 package com.kms.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.kms.exam.demo.repository.ArticleRepository;
 import com.kms.exam.demo.vo.Article;
 
-@Service
+@Component
 public class ArticleService {
 
-		public int articlesLastId;
-		private List<Article> articles;
+	@Autowired
+	private ArticleRepository articleRepository;
 
-		public ArticleService() {
-			articlesLastId = 0;
-			articles = new ArrayList<>();
-			makeTestData();
-		}
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
+	}
 
-		public void makeTestData() {
-			for (int i = 1; i <= 10; i++) {
-				int id = articlesLastId + 1;
-				String title = "제목" + i;
-				String body = "내용" + i;
+	public Article writeArticle(String title, String body) {
+		// TODO Auto-generated method stub
+		return articleRepository.writeArticle(title, body);
+	}
 
-			}
-		}
+	public List<Article> getArticles() {
+		// TODO Auto-generated method stub
+		return articleRepository.getArticles();
+	}
 
-		public Article writeArticle(String title, String body) {
-			int id = articlesLastId + 1;
-			Article article = new Article(id, title, body);
+	public Article getArticle(int id) {
+		// TODO Auto-generated method stub
+		return articleRepository.getArticle(id);
+	}
 
-			articles.add(article);
-			articlesLastId = id;
+	public void deleteArticle(int id) {
+		// TODO Auto-generated method stub
+		articleRepository.deleteArticle(id);
+	}
 
-			return article;
-		}
-
-		public Article getArticle(int id) {
-			for (Article article : articles) {
-				if (article.getId() == id) {
-					return article;
-				}
-			}
-			return null;
-		}
-
-		public void deleteArticle(int id) {
-			Article article = getArticle(id);
-			articles.remove(article);
-		}
-		
-
-		public void modifyArticle(int id, String title, String body) {
-			Article article = getArticle(id);
-			article.setTitle(title);
-			article.setBody(body);
-		}
-
-		public List<Article> getArticles() {
-			return articles;
-		}
+	public void modifyArticle(int id, String title, String body) {
+		// TODO Auto-generated method stub
+		Article article = getArticle(id);
+		article.setTitle(title);
+		article.setBody(body);
+	}
 }
