0e44771d43596b21851bf2bfc7ed3d4869213b3e
kimminsic <alstlr1135@gmail.com>
2022-07-01 19:17:56 +0900

    회원가입 완료 후. 생성된 회원 보여주기

--- src/main/java/com/kms/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kms/exam/demo/controller/UsrMemberController.java
@@ -5,24 +5,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kms.exam.demo.service.MemberService;
 import com.kms.exam.demo.vo.Article;
+import com.kms.exam.demo.vo.Member;
 
 @Controller
 public class UsrMemberController {
+	@Autowired
 	private MemberService memberService;
-public UsrMemberController(MemberService memberService) {
-	this.memberService=memberService;
-}
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		return "성공";
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		Member member = memberService.getMemberById(id);
+		
+		return member;
 	}
 }
