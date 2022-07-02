48bcdbca8bbdc650096cce3303103ebd34e6804b
kimminsic <alstlr1135@gmail.com>
2022-07-02 23:00:17 +0900

    resultData

--- src/main/java/com/kms/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kms/exam/demo/controller/UsrArticleController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kms.exam.demo.service.ArticleService;
+import com.kms.exam.demo.util.Ut;
 import com.kms.exam.demo.vo.Article;
+import com.kms.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -21,14 +23,15 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 		
 		if(article==null) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다", id));
 		}
 		
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다", id),article);
 	}
 	
 	@RequestMapping("/usr/article/doAdd")
--- src/main/java/com/kms/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kms/exam/demo/controller/UsrMemberController.java
@@ -51,14 +51,14 @@ public class UsrMemberController {
 
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
+		
 		if (id == -1) {
-			return Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.",loginId);
+			return Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId);
 		}
-		
-		if(id==-2) {
-			return Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.",name,email);
+
+		if (id == -2) {
+			return Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email);
 		}
-		
 
 		Member member = memberService.getMemberById(id);
 
