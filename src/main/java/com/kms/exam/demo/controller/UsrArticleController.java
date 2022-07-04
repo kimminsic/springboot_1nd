649b4b54a6b0580b7f10fe15251ec0b7e7a72412
kimminsic <alstlr1135@gmail.com>
2022-07-04 17:48:59 +0900

    doAdd ResultData 적용

--- src/main/java/com/kms/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kms/exam/demo/controller/UsrArticleController.java
@@ -47,7 +47,7 @@ public class UsrArticleController {
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
 		int id = writeArticleRd.getData1();
 		Article article = articleService.getArticle(id);
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+		return ResultData.newData(writeArticleRd,article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")
